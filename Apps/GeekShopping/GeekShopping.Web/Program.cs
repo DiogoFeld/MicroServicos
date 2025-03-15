c4d460bf832882f787b43be625129038e607c49c
Diogo Feld <cfeld.diogo@gmail.com>
2025-03-15 00:02:05 -0300

    Finished First API

--- Apps/GeekShopping/GeekShopping.Web/Program.cs
+++ Apps/GeekShopping/GeekShopping.Web/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.IService;
+
 namespace GeekShopping.Web
 {
     public class Program
@@ -8,6 +11,10 @@ namespace GeekShopping.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddHttpClient<IProductService, ProductService>(c =>
+             c.BaseAddress = new Uri(builder.Configuration["ServiveUrls:ProductAPI"])
+             );
 
             var app = builder.Build();
 
